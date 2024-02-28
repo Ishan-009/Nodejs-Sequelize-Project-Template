6291d4df7909c31b9189e578751e656dc28b82d0
Ishan-009 <ishanmoorjmalani009@gmail.com>
2024-02-28 17:10:22 +0530

    Adding Sequelize Project Starter Template

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { ServerConfig, Logger } = require("./config/index");
+const express = require("express");
+const apiRoutes = require("./routes/index");
+const app = express();
+const Cron = require("./utils/common/cron-jobs");
+app.use(express.json());
+// to help express read json body
+app.use(express.urlencoded({ extended: true }));
+// to read url encodeed stuff
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Server Listening on port ${ServerConfig.PORT}`);
+  Logger.info("Successfully started the server");
+  Cron();
+});
